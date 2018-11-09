a9fb91bab643ecd1ef2fdcf4dddea365555de94e
TheNoodleMoose <cjh605@gmail.com>
2018-11-09 10:12:17 -0500

    Added new languages to guess

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,7 +2,7 @@
 var game = {
     //holds values to make sure the player is
     alphabet: ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'],
-    word: ["ruby", "mongodb", "javascript", "python", "html", "css", "jquery", "react", "express", "node"],
+    word: ["ruby", "mongodb", "javascript", "python", "html", "css", "jquery", "react", "express", "node", "java", "rust", "php", "markdown"],
     answer: "",
     guessesLeft: 9,
     chosenWordArray: [],
